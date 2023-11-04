18d37730f2c76a38642f04d2f221ce5d733f32cc
krshubham1283 <krshubham1283@gmail.com>
2023-11-05 04:31:42 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css'
+
+import ProfileCard from "./ProfileCard";
+import AlexaImg from "./images/alexa.png";
+import CortanaImg from "./images/cortana.png";
+import SiriImg from "./images/siri.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistance</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImg} description='wiwbefw wue uieguiwe uiweg weg'/>
+            </div>
+            <div className="column is-4">
+              <ProfileCard title='Cortana' handle='@cortana32' image={CortanaImg} description='wenoifuwe uwe fuiwe uwe iwueg eg'/>
+            </div>
+            <div className="column is-4">
+              <ProfileCard title='Siri' handle='@siri33' image={SiriImg} description='iwuebfiu iuwe fuiwpa we iuwef woef wefi'/>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
